e6ec32a23263cbedcabe95041ab24a44ad43860a
Shaun Price <shaun@priceconsulting.biz>
2019-03-30 23:51:09 +1100

    Implemented point cloud from disparity image
    Implemented the point cloud from the disparity image. I had to remove
    scalling of the disparity image because it distorts the point cloud. The
    scale is not likely to be correct as I had to divide the 3D surface
    image from cv2.reprojectImageTo3D to get it close.

--- RoTVision/ros-disparity.py
+++ RoTVision/ros-disparity.py
@@ -12,18 +12,25 @@ import numpy as np
 import picamera
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import matplotlib
-from sklearn.preprocessing import normalize
 import roslib
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from sensor_msgs import point_cloud2
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Header
 from cv_bridge import CvBridge, CvBridgeError
+import rosgraph
 
 ################################################################
 # Initialise the Raspberry Pi video
 ################################################################
-os.system("sudo modprobe bcm2835-v4l2")
+
+################################################################
+# Either run the following command before running the script:
+#   sudo modprobe bcm2835-v4l2
+# or add bcm2835-v4l2 to the /etc/modules file
+################################################################
 
 ################################################################
 # Image Parameters
@@ -31,14 +38,8 @@ os.system("sudo modprobe bcm2835-v4l2")
 
 width = 640
 height = 480
-
-scaledWidth = 480
-scaledHeight = 360
-
-crop = 0
-
+crop = 50
 img_size = (width,height)
-rateCounter = 0
 
 ################################################################
 # Image Rectification Parameters
@@ -70,9 +71,9 @@ mtxTranslation = np.array(
 # High speed camera class
 ################################################################
 class PiVideoStream:
-    def __init__(self, resolution=(width*2, height), framerate=32):
+    def __init__(self, resolution=(width, height*2), framerate=32):
         # initialize the camera and stream
-        self.camera = PiCamera(stereo_mode="side-by-side")
+        self.camera = PiCamera(stereo_mode="top-bottom")
         self.camera.resolution = resolution
         self.camera.framerate = framerate
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
@@ -89,11 +90,22 @@ class PiVideoStream:
         return self
 
     def update(self):
+        self.camera_pub = rospy.Publisher("camera",Image,queue_size=20)
+        self.bridge = CvBridge()
+
         # keep looping infinitely until the thread is stopped
         for frame in self.stream:
             # grab the frame from the stream and clear the stream in
             # preparation for the next frame
             self.frame = frame.array
+
+            # Take the left image and send it as the camera image
+            camera = cv2.rotate(self.frame[height:height*2-1,0:width],cv2.ROTATE_180)
+
+            try:
+                self.camera_pub.publish(self.bridge.cv2_to_imgmsg(camera, "bgr8"))
+            except CvBridgeError as e:
+                print(e)
 
             self.rawCapture.truncate(0)
             # if the thread indicator variable is set, stop the thread
@@ -103,6 +115,7 @@ class PiVideoStream:
                 self.rawCapture.close()
                 self.camera.close()
                 return
+
     def read(self):
         # return the frame most recently read
         return self.frame
@@ -120,8 +133,10 @@ class Disparity:
         ################################################################
         print("Initialise ROS Bridge")
 
-        self.disparity_pub = rospy.Publisher("disparity_image",Image,queue_size=10)
-        self.stereo_pub = rospy.Publisher("stereo_image",Image,queue_size=10)
+        self.disparity_pub = rospy.Publisher("stereo/disparity",Image,queue_size=5)
+        self.left_pub = rospy.Publisher("stereo/left_image",Image,queue_size=5)
+        self.right_pub = rospy.Publisher("stereo/right_image",Image,queue_size=5)
+        self.pointcloud_pub = rospy.Publisher("stereo/pointcloud",PointCloud2,queue_size=5)
         self.bridge = CvBridge()
 
         ################################################################
@@ -141,15 +156,15 @@ class Disparity:
         ################################################################
         print("Creating the Stereo Matcher")
         # SGBM Parameters -----------------
-        window_size = 5      # 5           # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-        min_disp = -48                    # -48 for 320x240 and 240x180, -96 for 640x480
-        num_disp = 48-min_disp             # 48 for 320x240 and 240x180, 96 for 640x480
+        window_size = 5      # 5      # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+        min_disp = -48                # -48 for 320x240 and 240x180, -96 for 640x480
+        num_disp = 48-min_disp        # 48 for 320x240 and 240x180, 96 for 640x480
 
         self.left_matcher = cv2.StereoSGBM_create(
                 minDisparity=min_disp,
-                numDisparities=num_disp,             # max_disp has to be dividable by 16 f. E. HH 192, 256
+                numDisparities=num_disp,       # max_disp has to be dividable by 16 f. E. HH 192, 256
                 blockSize=5, # 5
-                P1=8 * 3 * window_size ** 2,    # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+                P1=8 * 3 * window_size ** 2,   # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
                 P2=32 * 3 * window_size ** 2,
                 disp12MaxDiff=-1,
                 uniquenessRatio=5,  # 1
@@ -170,7 +185,6 @@ class Disparity:
         # FILTER Parameters
         lmbda = 16000
         sigma = 2.0   #1.2
-        visual_multiplier = 1.0
 
         self.wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=self.left_matcher)
         self.wls_filter.setLambda(lmbda)
@@ -180,8 +194,7 @@ class Disparity:
 
         print("Initialisation Complete")
 
-    def start(self, videoStream):
-        self.videoStream = videoStream
+    def start(self):
         Thread(target=self.update, args=()).start()
         return self
 
@@ -189,100 +202,132 @@ class Disparity:
         print("Starting Disparity Loop")
         # keep looping infinitely until the thread is stopped
         
+        print("Start the capture thread")
+        self.videoStream = PiVideoStream().start()
+
+        print("Waiting")
+        time.sleep(2)
+
+        print("Starting Capture Loop")
         while True:
-            image = np.empty((height * width*2 * 3,), dtype=np.uint8)
+            image = np.empty((height*2 * width * 3), dtype=np.uint8)
             image = self.videoStream.read()
 
-            ################################################################
-            if image.size:
-                greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
-                greyLeft = greyImage[0:height,0:width-1]
-                greyRight = greyImage[0:height,width:width*2-1]
-
-                c, r = greyLeft.shape
-
-                time.sleep(0)
-
+            try:
                 ################################################################
-                #Rectify the images:
-                leftImageRectified = cv2.remap(greyLeft, self.xmap1, self.ymap1, cv2.INTER_LINEAR)
-                rightImageRectified = cv2.remap(greyRight, self.xmap2, self.ymap2, cv2.INTER_LINEAR)
-                leftImageRectifiedResized = cv2.resize(leftImageRectified , (scaledWidth,scaledHeight))
-                rightImageRectifiedResized = cv2.resize(rightImageRectified , (scaledWidth,scaledHeight))
+                if image is not None:
+                    greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            
+                    greyRight = greyImage[0:height-1,0:width]
+                    greyLeft = greyImage[height:height*2-1,0:width]
 
-                leftImageRectifiedResizedRotated = cv2.rotate(leftImageRectifiedResized, cv2.ROTATE_180)
-                rightImageRectifiedResizedRotated = cv2.rotate(rightImageRectifiedResized, cv2.ROTATE_180)
-
-                ################################################################
-                # Now we can compute the disparities and convert the resulting images to the desired int16 format or how OpenCV names it: CV_16S for our filter:
-                displ = self.left_matcher.compute(leftImageRectifiedResizedRotated, rightImageRectifiedResizedRotated)  # .astype(np.float32)/16
-                dispr = self.right_matcher.compute(rightImageRectifiedResizedRotated, leftImageRectifiedResizedRotated)  # .astype(np.float32)/16
-
-                displ = np.int16(displ)
-                dispr = np.int16(dispr)
-                filteredImg = self.wls_filter.filter(displ, leftImageRectifiedResizedRotated, None, dispr)  # important to put "leftImageRectifiedResizedRotated" here!!!
-
-                ################################################################
-                # Finally if you show this image with imshow() you may not see anything. This is due to values being not normalized to a 8-bit format. So lets fix this by normalizing our depth map:
-                cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
-
-                filteredImg = filteredImg[0:height, crop:width-crop]
-
-                filteredImg = np.uint8(filteredImg)
-                filteredImg = cv2.rotate(filteredImg,cv2.ROTATE_180)
-
-                try:
-                    self.disparity_pub.publish(self.bridge.cv2_to_imgmsg(cv2.resize(filteredImg,(width-crop,height)), "mono8"))
-                    #print("Disparity Image Sent")
-                except CvBridgeError as e:
-                    print(e)
-
-                try:
-                    # Had to rotate the image. Need to clean this up so image isn't rotated to start with.
-                    self.stereo_pub.publish(self.bridge.cv2_to_imgmsg(cv2.resize(cv2.rotate(image, cv2.ROTATE_180),(width,height)), "bgr8"))
-                    #print("Stereo Image Sent")
-                except CvBridgeError as e:
-                    print(e)
-
-                ################################################################
-                # if the thread indicator variable is set, stop the thread
-                if self.stopped:
-                    return
-                else:
                     time.sleep(0)
-            else:
-                time.sleep(0.5)
-                print("No image to process")
- 
+
+                    ################################################################
+                    #Rectify the images:
+                    leftImageRectified = cv2.remap(greyLeft, self.xmap1, self.ymap1, cv2.INTER_LINEAR)
+                    rightImageRectified = cv2.remap(greyRight, self.xmap2, self.ymap2, cv2.INTER_LINEAR)
+
+                    leftImageRectifiedRotated = cv2.rotate(leftImageRectified, cv2.ROTATE_180)
+                    rightImageRectifiedRotated = cv2.rotate(rightImageRectified, cv2.ROTATE_180)
+
+                    ################################################################
+                    # Now we can compute the disparities and convert the resulting images to the desired int16 format or how OpenCV names it: CV_16S for our filter:
+                    displ = self.left_matcher.compute(leftImageRectifiedRotated, rightImageRectifiedRotated)  # .astype(np.float32)/16
+                    dispr = self.right_matcher.compute(rightImageRectifiedRotated, leftImageRectifiedRotated)  # .astype(np.float32)/16
+
+                    displ = np.int16(displ)
+                    dispr = np.int16(dispr)
+                    filteredImg = self.wls_filter.filter(dispr, leftImageRectifiedRotated, None, dispr)  # important to put "leftImageRectifiedRotated" here!!!
+
+                    ################################################################
+                    # Finally if you show this image with imshow() you may not see anything. This is due to values being not normalized to a 8-bit format. So lets fix this by normalizing our depth map:
+                    cv2.normalize(src=filteredImg, dst=filteredImg, alpha=0, beta=255,  norm_type=cv2.NORM_MINMAX)
+
+                    cropDispLeft = width - crop
+                    cropDispRight = crop
+                    # Note that the image is rotated 180 at this point
+                    filteredImg = filteredImg[0:height, cropDispRight:cropDispLeft]
+                    filteredImg = np.uint8(filteredImg)
+                    disparityImg = cv2.rotate(filteredImg,cv2.ROTATE_180)
+
+                    ################################################################
+                    # Send Disparity Image
+                    try:
+                        self.disparity_pub.publish(self.bridge.cv2_to_imgmsg(disparityImg, "mono8"))
+                    except CvBridgeError as e:
+                        print(e)
+
+                    # Send Rectified Left Greyscale Image
+                    try:
+                        self.left_pub.publish(self.bridge.cv2_to_imgmsg(leftImageRectified, "mono8"))
+                    except CvBridgeError as e:
+                        print(e)
+
+                    # Send Rectified Right Greyscale Image
+                    try:
+                        self.right_pub.publish(self.bridge.cv2_to_imgmsg(rightImageRectified, "mono8"))
+                    except CvBridgeError as e:
+                        print(e)
+
+                    # Create the pointcloud
+                    pointcloudImg = np.rot90(disparityImg)
+                    points = cv2.reprojectImageTo3D(pointcloudImg,self.Q)
+                    pointsOut = points.reshape((-1,3))
+                    pointsOut = pointsOut / 8 # Convert to meters
+
+                    header = Header()
+                    header.stamp = rospy.Time.now()
+                    header.frame_id = "stereo/pointcloud"
+
+                    pointCloud = point_cloud2.create_cloud_xyz32(header, pointsOut)
+
+                    # Send Point Cloud
+                    try:
+                        self.pointcloud_pub.publish(pointCloud)
+                    except CvBridgeError as e:
+                        print(e)
+
+                    ################################################################
+                    # if the thread indicator variable is set, stop the thread
+                    if self.stopped:
+                        return
+                    else:
+                        time.sleep(0)
+                else:
+                    time.sleep(0.5)
+                    print("No image to process")
+            # Catch when an image error is thrown
+            except AttributeError as e:
+                print(e)   
+                raise e  
+            except KeyboardInterrupt as ki:
+                print("Exiting Loop")
+                raise ki
+
     def stop(self):
+        print("Closing Camera Stream")
+        self.videoStream.stop()
+
         # indicate that the thread should be stopped
         self.stopped = True
 
 ################################################################
+
 def main(args):
-    
-    print("Start the capture thread")
-    videoStream = PiVideoStream().start()
-
-    print("Waiting")
-    time.sleep(2)
-
     print("Start the disparity thread")
-    disp = Disparity().start(videoStream)
+    disp = Disparity().start()
     
     rospy.init_node('Disparity', anonymous=True)
 
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Stopping")
-        print("Closing Disparity")
-        disp.stop()
-        print("Closing Camera Stream")
-        videoStream.stop()
-        print("Stopped")
-        os._exit(0)
+    rospy.spin()
+    
+    print("Stopping")
+    print("Closing Disparity")
+    disp.stop()
+    
+    print("Stopped")
+    os._exit(0)
 
 if __name__ == '__main__':
     main(sys.argv)
